d22b4b1cc5cdf48816f67e2cc37e28cae8d679a1
Maxime MEDAL <medal.max@gmail.com>
2022-01-31 15:51:46 +0100

    lancement des serveurs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
 const express = require('express');
+
+const { SERVER_PORT } = process.env;
 
 
 const app = express();
 
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Express");
+});
+
+
+// server setup
+const server = app.listen(SERVER_PORT, () => {
+  console.log(`Server running on port ${SERVER_PORT}`);
+});
